ae957492c30a5bbd34bc18a5d6f14e302579fdb7
Felix Pojtinger <felix@pojtinger.com>
2021-02-10 22:54:45 +0000

    feat: Add rebass preset

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "@emotion/react";
+import theme from "@rebass/preset";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
